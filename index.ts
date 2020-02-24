5314822739631691890325c3fb0ad677b3953b40
Eric Harm <ericharm@Erics-MacBook-Pro-2.local>
2020-02-24 09:16:40 -0500

    empty lines

--- index.ts
+++ index.ts
@@ -4,4 +4,4 @@ import GameState from './src/states/GameState.ts'
 const app: Application = new Application()
 const game: GameState = new GameState()
 app.stateStack.push(game)
-app.init({ frameRate: 1 })
+app.init({ frameRate: 60 })
--- src/states/GameState.ts
+++ src/states/GameState.ts
@@ -5,29 +5,28 @@ import Vector2 from '../Vector2.ts'
 class GameState extends State {
 
   processEvents(players: Player[]): void {
-
   }
 
-
   processRealtimeInput(players: Player[]): void {
-
   }
 
   update(deltaTime: number): void {
-
   }
 
   draw(canvas: CanvasRenderingContext2D): void {
-    canvas.fillStyle = '#0000ff'
-    canvas.rotate(20 * Math.PI / 180)
+    // canvas.rotate(20 * Math.PI / 180)
     let position: Vector2 = { x: 300, y: 100 }
-    let size: Vector2 = { x: 100, y: 100 }
-    canvas.fillRect(position.x, position.y, size.x, size.y)
+    let size: Vector2 = { x: 50, y: 50 }
+    canvas.strokeStyle = '#00ff00'
+    canvas.beginPath()
+    canvas.ellipse(position.x, position.y, size.x, size.y, 20, 0, 300)
+    canvas.stroke()
 
     canvas.setTransform(1, 0, 0, 1, 0, 0)
 
     position = { x: 400, y: 200 }
     size = { x: 100, y: 100 }
+    canvas.fillStyle = '#0000ff'
     canvas.fillRect(position.x, position.y, size.x, size.y)
   }
 }
